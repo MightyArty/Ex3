1220a48865bbca0e3519af65cc1ab69825934ce2
davidy1995 <david.yusopov@msmail.ariel.ac.il>
2021-12-24 17:12:50 +0200

    DiGraph tests and plot updates

--- src/plot.py
+++ src/plot.py
@@ -6,6 +6,7 @@ from Edge import *
 from matplotlib.patches import ConnectionPatch
 from DiGraph import *
 from Node import Node
+
 fig, (ax1) = plt.subplots(figsize=(5, 5))
 graph = DiGraph()
 graphAgo = GraphAlgo()
@@ -20,25 +21,24 @@ graph.add_node(2, b)
 graph.add_node(3, c)
 locations = []
 nodes = get.get_all_v()
-id = []
-keys = get.graph.nodesMap.keys()
+
+keys = graph.nodesMap.keys()
 
 # set the id & loc into keys
 for i in keys:
-    id.append(graph.nodesMap.get(i).id)
     locations.append(graph.nodesMap.get(i).pos)
 
 x_list = [loc[0] for loc in locations]  # store x values
 y_list = [loc[1] for loc in locations]  # store y values
 z_list = [loc[2] for loc in locations]  # store z values
-
+id = [ids for ids in nodes]  # the python way to loop al the ids into the list
 xyA = (x_list[0], y_list[0])
 xyB = (x_list[1], y_list[1])
 xyC = (x_list[2], y_list[2])
 
 plt.scatter(x_list, y_list, color='green')  # green node dot
 for i in range(3):
-    plt.text(x_list[i], y_list[i], f'{id[i]}', ha='center', fontsize=12) # add node id
+    plt.text(x_list[i], y_list[i], f'{id[i]}', ha='center', fontsize=12)  # add node id
 
 # for i in keys:
 #     childes = graph.all_out_edges_of_node(id[i]).values(i)
@@ -58,11 +58,15 @@ plt.ylim(0, 100)  # limit Y
 plt.show()
 
 if __name__ == '__main__':
-    # graph.add_edge(1,2,5)
-    # graph.add_edge(1, 3, 15)
-    # print(graph.all_out_edges_of_node(1).keys())
-    # print (childes)
-    # print(childes)
-    print(nodes)
-    # print(get.get_all_v())
+# x_list_new = [xs["pos"] for xs in nodes]
 
+# print(nodes['Node id'])
+# print(nodes.pop('pos'))
+#     print(nodes['pos'])
+    print(nodes.get('pos'))
+    print(nodes.values())
+# print(locations)
+# print(nodes)
+# print(keys)
+
+# print(get.get_all_v())
---
+++ src/test_DiGraph.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+from DiGraph import *
+
+graph = DiGraph()
+a = (1,2,0)
+b = (2,4,0)
+c = (6,3,0)
+graph.add_node(0,a)
+graph.add_node(1,b)
+graph.add_node(2,c)
+graph.add_edge(2,4,5) # one edge 2 -- 4
+
+class TestDiGraph(TestCase):
+    def test_v_size(self):
+        e = graph.v_size()
+        self.assertTrue(e,3)
+
+    def test_e_size(self):
+        e = graph.e_size()
+        self.assertTrue(e,1)
+
+
+    def test_get_all_v(self):
+        e = graph.get_all_v()
+        self.assertTrue(e,graph.nodesMap)
+
+    def test_all_in_edges_of_node(self): # NEED TO FIX
+        e = graph.all_in_edges_of_node(2)
+        # self.assertTrue(e,1)
+        print(e)
+
+    def test_all_out_edges_of_node(self):
+        self.fail()
+
+    def test_get_mc(self):
+        e = graph.get_mc()
+        self.assertTrue(e,4)
+
+
+    def test_add_edge(self):
+        before = graph.e_size()
+        graph.add_edge(2,6,1)
+        e = graph.e_size()
+        self.assertNotEqual(before,e)
+
+
+    def test_add_node(self):
+        before = graph.v_size() #3
+        temp = (10,10,0)
+        graph.add_node(4,temp)
+        e = graph.v_size() # 4
+        self.assertNotEqual(before, e)
+
+
+    def test_remove_node(self): # NEED TO FIX
+        before = graph.v_size() # 3
+        graph.remove_node(2)
+        e = graph.v_size() #2
+        self.assertNotEqual(e,before)
+
+
+
+    def test_remove_edge(self):
+        before = graph.e_size() #1
+        graph.remove_edge(2,4)
+        e = graph.e_size() #0
+        self.assertNotEqual(e,before)
+
